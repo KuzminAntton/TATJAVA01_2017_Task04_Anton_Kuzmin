9e44642bfcdb914bb58a7b152e9f1baee2bb561e
KuzminAntton <anttonkuzmin@gmail.com>
2017-02-16 20:18:58 +0300

    Update NewsValidator.java

--- src/main/java/com/epam/task4/util/NewsValidator.java
+++ src/main/java/com/epam/task4/util/NewsValidator.java
@@ -1,10 +1,8 @@
 package com.epam.task4.util;
 
-<<<<<<< HEAD
+
 import com.epam.task4.bean.News;
 
-=======
->>>>>>> 928a172da525de4ed9c409d06b8a3334992c4d1b
 public final class NewsValidator {
     /**
      * Check that news is correct.
@@ -21,8 +19,6 @@ public final class NewsValidator {
         return res;
     }
 
-
-<<<<<<< HEAD
     public static boolean newsValidateNotNull(String title) {
         boolean res = true;
         if(title.isEmpty()) {
@@ -66,6 +62,4 @@ public final class NewsValidator {
 
 
 
-=======
->>>>>>> 928a172da525de4ed9c409d06b8a3334992c4d1b
 }
