3374ea6d5d16c69aac87524f511f9ed51f1b928f
Kuzmin Anton <kuzminantton@gmail.com>
2017-02-19 22:08:36 +0300

    fix

--- src/main/java/com/epam/task4/controller/commands/WrongCommand.java
+++
@@ -1,9 +0,0 @@
-package com.epam.task4.controller.commands;
-
-public class WrongCommand implements Command {
-
-    @Override
-    public String execute(String request) {
-        return "Command is incorrect";
-    }
-}
--- src/main/java/com/epam/task4/controller/commands/WrongRequest.java
+++
@@ -1,8 +0,0 @@
-package com.epam.task4.controller.commands;
-
-public class WrongRequest implements Command {
-    @Override
-    public String execute(String request) {
-        return "Incorrect request";
-    }
-}
